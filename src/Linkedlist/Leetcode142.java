3a8d92d9cc4572a39c675999675f2142fd878002
hefun <3594313357@qq.com>
2023-03-07 12:00:59 +0800

    Leetcode142

--- src/Linkedlist/Leetcode142.java
+++ src/Linkedlist/Leetcode142.java
@@ -8,6 +8,34 @@ public class Leetcode142 {
     }
 
     public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+        ListNode slow = head, fast = head.next.next;
 
+        while (slow != fast && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast == null || fast.next == null)
+            return null;
+
+        int len = 1;
+        fast = fast.next;
+        while (slow != fast) {
+            fast = fast.next;
+            len++;
+        }
+
+        slow = fast = head;
+        for (int i = 0; i < len; i++) {
+            fast = fast.next;
+        }
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return fast;
     }
 }
